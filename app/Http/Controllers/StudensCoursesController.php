2dce3d066547beca8f4078da71d8273401cfb82f
jstcode99 <jstorres0211@gmail.com>
2021-11-03 16:56:24 -0500

    Formulario de registro de studen course, revisar comentarios para que sepas que falta hacer

--- app/Http/Controllers/StudensCoursesController.php
+++ app/Http/Controllers/StudensCoursesController.php
@@ -6,13 +6,37 @@ use Illuminate\Http\Request;
 use App\Models\studensCourses;
 use App\Models\User;
 
-class StudensCoursesController extends Controller
-{
+class StudensCoursesController extends Controller {
     public function index(){
-
         $users = User::with('courses')->where('rol', 'Estudiante')->get();
         // $users = $user->courses;
         return response()->json(['status' => 200, 'response' => $users]);
+    }
 
+    public function store(Request $request){
+        // Aqui haces la funcion store commun y corriente apuntado al modelo studensCourses
+        $rules = [
+            'course_id' => 'required',
+            'user_id' => 'required',
+
+        ];
+        $messages = [
+            'course_id.required' => 'course_id is required to store!',
+            'user_id.required' => 'user_id is required to store!',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json([
+                'status' => 201,
+                'response' => $errors,
+            ]);
+        } else {
+            $course = new studensCourses;
+            $course->create($request->all());
+            return response()->json(['status' => 200, 'response' => true]);
+        }
     }
 }
